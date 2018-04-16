6359e9c30acb5419b8799a7fa1a6744b1c24c890
Tulio Garcia <tuliohn97@hotmail.com>
2018-04-15 18:52:58 -0600

    lol
    ja

--- vistas/layouts/mainheader.php
+++ vistas/layouts/mainheader.php
@@ -23,14 +23,14 @@
                             <!-- The user image in the navbar-->
                             <img src="./img/user2-160x160.jpg" class="user-image" alt="User Image"/>
                             <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                            <span class="hidden-xs">Pedro Picapiedra</span>
+                            <span class="hidden-xs">Administrador</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- The user image in the menu -->
                             <li class="user-header">
                                 <img src="./img/user2-160x160.jpg" class="img-circle" alt="User Image" />
                                 <p>
-                                    Pedro Picapiedra
+                                    Administrador
                                     <small>Logueado desde Nov. 2017</small>
                                 </p>
                             </li>
